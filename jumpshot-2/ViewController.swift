aa927b74c3c51ae90745087ba10fc8760c87f910
Jonathan Yu <jonathan@jonathanyu.net>
2015-02-08 09:48:12 -0800

    Initial Commit

---
+++ jumpshot-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jumpshot-2
+//
+//  Created by Jonathan Yu on 2/8/15.
+//  Copyright (c) 2015 Jonathan Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
